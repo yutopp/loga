582849c9fabdc592106d1b46dd1f7a03597760a7
yutopp <yutopp@gmail.com>
2017-08-14 04:04:48 +0900

    fix filepath. setup formatter

--- example/log_example.ml
+++ example/log_example.ml
@@ -1,5 +1,9 @@
 let () =
   [%Loga.info "loglog"];
-  [%Loga.warning "%d" 42];
-  Printf.printf "LINE: %d\n" __LINE__;
+  (* 2017-08-14T03:58:33+09:00 [INFO] (Log_example:2) loglog *)
+
+  let n = 42 in
+  [%Loga.warning "N = %d" n];
+  (* 2017-08-14T04:00:41+09:00 [WARNING] (Log_example:6) N = 42 *)
+
   ()
--- src/loga.ml
+++ src/loga.ml
@@ -1,2 +1,71 @@
-let log (_severity, module_, line) fmt =
-  Printf.printf ("(%s:%d) " ^^ fmt ^^ "\n") module_ line;
+module Severity =
+  struct
+    type t =
+      | Emergency
+      | Alert
+      | Critical
+      | Error
+      | Warning
+      | Notice
+      | Info
+      | Debug
+
+    let of_int n =
+      match n with
+      | 0 -> Emergency
+      | 1 -> Alert
+      | 2 -> Critical
+      | 3 -> Error
+      | 4 -> Warning
+      | 5 -> Notice
+      | 6 -> Info
+      | 7 -> Debug
+      | _ -> failwith "unexpected"
+
+    let string_of k =
+      match k with
+      | Emergency   -> "EMERGENCY"
+      | Alert       -> "ALERT"
+      | Critical    -> "CRITICAL"
+      | Error       -> "ERROR"
+      | Warning     -> "WARNING"
+      | Notice      -> "NOTICE"
+      | Info        -> "INFO"
+      | Debug       -> "DEBUG"
+  end
+
+let kfprint_datetime k formatter =
+  let tz, _ = Unix.mktime (Unix.gmtime 0.0) in
+  let Unix.{
+    tm_year = year;
+    tm_mon = month;
+    tm_mday = mday;
+    tm_hour = hours;
+    tm_min = minutes;
+    tm_sec = seconds;
+  } = Unix.localtime (Unix.gettimeofday ()) in
+  let tz_hours = -(int_of_float tz) / 3600 in
+  let tz_minutes = -(int_of_float tz) / 60 mod 60 in
+  Format.kfprintf k formatter
+                  "%04d-%02d-%02dT%02d:%02d:%02d%0+3d:%02d "
+                  (year + 1900) (month + 1) mday
+                  hours minutes seconds tz_hours tz_minutes
+
+let default_printer (severity, module_, line) formatter fmt =
+  let k formatter =
+    Format.fprintf formatter "\n%!"
+  in
+  let k formatter =
+    Format.kfprintf k formatter fmt
+  in
+  let k formatter =
+    Format.kfprintf k formatter "(%s:%d) " module_ line
+  in
+  let k formatter =
+    Format.kfprintf k formatter "[%s] " (Severity.string_of severity)
+  in
+  kfprint_datetime k formatter
+
+let log (severity_i, module_, line) fmt =
+  default_printer (Severity.of_int severity_i, module_, line)
+                  Format.std_formatter fmt
